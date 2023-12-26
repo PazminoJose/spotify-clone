9a66742fba5290084a17d0d715162c9a6ef13d96
José Pazmiño <pepe.paz0399@gmail.com>
2023-12-26 10:16:03 -0500

    feat: create Player component

---
+++ src/components/react/Player.tsx
@@ -0,0 +1,41 @@
+import { usePlayerStore } from "@/store/playerStore";
+import { useEffect } from "react";
+import CurrentSong from "./CurrentSong";
+import PlayerController from "./PlayerController";
+import VolumeControl from "./VolumeControl";
+
+export function Player() {
+  const { isPlaying, currentMusic, volume, audio } = usePlayerStore((state) => state);
+
+  useEffect(() => {
+    isPlaying && audio !== null ? audio?.play() : audio?.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    if (audio !== null) audio.volume = volume;
+  }, [volume]);
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song && audio !== null) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audio.src = src;
+      audio.volume = volume;
+      audio.play();
+    }
+  }, [currentMusic]);
+
+  return (
+    <div className="z-50 flex h-full w-full flex-row justify-between bg-black px-1">
+      <div className="w-[250px]">
+        <CurrentSong {...currentMusic.song} />
+      </div>
+      <div className="grid flex-1 place-content-center gap-4">
+        <PlayerController />
+      </div>
+      <div className="grid place-content-center">
+        <VolumeControl />
+      </div>
+    </div>
+  );
+}
